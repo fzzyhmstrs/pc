c8487655f3853ea07e8131557b86c8b2a154a0ce
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-16 15:45:29 -0400

    Object2Int non-linked

--- src/main/java/me/fzzyhmstrs/particle_core/interfaces/CachedLightProvider.java
+++ src/main/java/me/fzzyhmstrs/particle_core/interfaces/CachedLightProvider.java
@@ -1,13 +1,10 @@
 package me.fzzyhmstrs.particle_core.interfaces;
 
-import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenCustomHashMap;
-import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.util.math.BlockPos;
-
-import java.util.HashMap;
 
 public interface CachedLightProvider {
 
-    Object2IntLinkedOpenHashMap<BlockPos> particle_core_getCache();
+    Object2IntOpenHashMap<BlockPos> particle_core_getCache();
 
 }
