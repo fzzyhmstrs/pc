08f4c5947cf16850b7247d7a5acd3fd9e0b58daa
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-16 15:45:29 -0400

    Object2Int non-linked

--- src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCachedLightMixin.java
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCachedLightMixin.java
@@ -1,5 +1,7 @@
 package me.fzzyhmstrs.particle_core.mixins;
 
+import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import me.fzzyhmstrs.particle_core.interfaces.CachedLightProvider;
@@ -30,10 +32,10 @@ import java.util.HashMap;
 public class ParticleManagerCachedLightMixin implements CachedLightProvider {
 
     @Unique
-    private final HashMap<BlockPos,Integer> cachedLightMap = new HashMap<BlockPos,Integer>();
+    private final Object2IntOpenHashMap<BlockPos> cachedLightMap = new Object2IntOpenHashMap<>(64, 0.75f);
 
     @Override
-    public HashMap<BlockPos, Integer> particle_core_getCache() {
+    public Object2IntOpenHashMap<BlockPos> particle_core_getCache() {
         return cachedLightMap;
     }
 
