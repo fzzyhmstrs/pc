f181864c427e5fd24aa74006854bd9422a899bb8
TheEnderCore <theendercore@gmail.com>
2025-07-02 19:09:35 +0300

    1.21.6

--- src/main/java/me/fzzyhmstrs/particle_core/mixins/WorldRendererFrustumMixin.java
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/WorldRendererFrustumMixin.java
@@ -1,6 +1,7 @@
 package me.fzzyhmstrs.particle_core.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import me.fzzyhmstrs.particle_core.interfaces.FrustumProvider;
@@ -10,8 +11,8 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.ObjectAllocator;
-import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -31,7 +32,7 @@ public class WorldRendererFrustumMixin {
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "render", at = @At(value = "CONSTANT", args = "stringValue=clear"), require = 0)
-    private void particle_core_passFrustumToParticleManager1(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci, @Local Frustum frustum){
+    private void particle_core_passFrustumToParticleManager1(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fog, Vector4f fogColor, boolean shouldRenderSky, CallbackInfo ci, @Local Frustum frustum){
         ((FrustumProvider)this.client.particleManager).particle_core_setFrustum(frustum);
     }
 }
