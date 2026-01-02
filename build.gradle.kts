828833607941d5fe8a65426fa75ee09b000a9eee
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-02 12:45:53 -0500

    0.2.7 build.gradle.kts upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,10 @@ repositories {
         name = "Jitpack"
         url = uri("https://jitpack.io")
     }
+    maven {
+        name = "FzzyMaven"
+        url = uri("https://maven.fzzyhmstrs.me/")
+    }
     mavenCentral()
 }
 dependencies {
@@ -56,7 +60,7 @@ dependencies {
     modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
 
     val fzzyConfigVersion: String by project
-    modImplementation("maven.modrinth:fzzy-config:$fzzyConfigVersion") {
+    modImplementation("me.fzzyhmstrs:fzzy_config:$fzzyConfigVersion+$minecraftVersion"){
         exclude("net.fabricmc.fabric-api")
     }
 
