d95fec68a9f056b222e1456c7d2c7f49057cca8c
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-02 12:47:14 -0500

    0.2.7 move light caching onto the particle, with the map cache queried on tick, not each frame. Block changes don't really happen between ticks anyways, and shouldn't be noticeable with dynamic lighting mods

---
+++ src/main/java/me/fzzyhmstrs/particle_core/interfaces/CachedLightPreparer.java
@@ -0,0 +1,9 @@
+package me.fzzyhmstrs.particle_core.interfaces;
+
+import net.minecraft.world.BlockRenderView;
+
+public interface CachedLightPreparer {
+
+    void particle_core_tickLightUpdate(BlockRenderView world);
+
+}
