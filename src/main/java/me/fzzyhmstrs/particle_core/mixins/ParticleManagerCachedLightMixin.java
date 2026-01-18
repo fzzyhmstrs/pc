8085e6c778d7be50e9fd1adceda01d63fd5ca8d3
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-18 00:30:03 -0500

    0.3.0 content. Many performance improvements and new features

--- src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCachedLightMixin.java
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCachedLightMixin.java
@@ -3,7 +3,6 @@ package me.fzzyhmstrs.particle_core.mixins;
 import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenCustomHashMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import me.fzzyhmstrs.particle_core.interfaces.CachedLightPreparer;
@@ -11,21 +10,16 @@ import me.fzzyhmstrs.particle_core.interfaces.CachedLightProvider;
 import me.fzzyhmstrs.particle_core.plugin.PcConditionTester;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.particle.EmitterParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Environment(EnvType.CLIENT)
 @Restriction(
@@ -36,23 +30,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ParticleManager.class)
 public class ParticleManagerCachedLightMixin implements CachedLightProvider {
 
-    @Shadow protected ClientWorld world;
     @Unique
-    private final Object2IntLinkedOpenHashMap<BlockPos> cachedLightMap = new Object2IntLinkedOpenHashMap<>(64, 0.75f);
+    private volatile ConcurrentHashMap<BlockPos, Integer> cachedLightMap = new ConcurrentHashMap<>(64, 0.75f);
 
     @Override
-    public Object2IntLinkedOpenHashMap<BlockPos> particle_core_getCache() {
+    public ConcurrentHashMap<BlockPos, Integer> particle_core_getCache() {
         return cachedLightMap;
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
-    private void particle_core_clearCache(CallbackInfo ci){
-        cachedLightMap.clear();
+    private void particle_core_clearCache(CallbackInfo ci) {
+        int size = cachedLightMap.size();
+        cachedLightMap = new ConcurrentHashMap<>(size, 0.75f);
     }
 
     @WrapOperation(method = "tickParticle", at = @At(value = "INVOKE", target = "net/minecraft/client/particle/Particle.tick ()V"))
-    private void particle_core_onEmitterParticleTick(Particle instance, Operation<Void> original){
-        ((CachedLightPreparer) instance).particle_core_tickLightUpdate(this.world);
+    private void particle_core_tickParticleLightUpdates(Particle instance, Operation<Void> original) {
+        ((CachedLightPreparer) instance).particle_core_tickLightUpdate();
         original.call(instance);
     }
 }
