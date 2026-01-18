dee8bcfae3a9abced2c3866284a81f8cfd04e5eb
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-18 00:30:03 -0500

    0.3.0 content. Many performance improvements and new features

---
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCountMixin.java
@@ -0,0 +1,35 @@
+package me.fzzyhmstrs.particle_core.mixins;
+
+import com.google.common.collect.EvictingQueue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fzzyhmstrs.particle_core.PcConfig;
+import me.fzzyhmstrs.particle_core.plugin.PcConditionTester;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.particle.ParticleTextureSheet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.function.Function;
+
+@Environment(EnvType.CLIENT)
+@Restriction(
+		require = {
+				@Condition(type = Condition.Type.TESTER, tester = PcConditionTester.class)
+		}
+)
+@Mixin(value = ParticleManager.class, priority = 100000)
+public class ParticleManagerCountMixin {
+
+	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "java/util/Map.computeIfAbsent (Ljava/lang/Object;Ljava/util/function/Function;)Ljava/lang/Object;"))
+	private Object particle_core_particleCount(Map<ParticleTextureSheet, Queue<Particle>> instance, Object key, Function<? super ParticleTextureSheet, ? extends Queue<Particle>> function, Operation<Queue<Particle>> original) {
+		return instance.computeIfAbsent((ParticleTextureSheet) key, (k) -> EvictingQueue.create(PcConfig.INSTANCE.getImpl().getMaxParticlesPerSheet().get()));
+	}
+}
