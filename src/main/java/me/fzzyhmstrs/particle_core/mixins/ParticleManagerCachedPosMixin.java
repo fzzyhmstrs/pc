2e684868661d0bf5a045da74ac57c7c7b5cb4d84
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-18 00:30:03 -0500

    0.3.0 content. Many performance improvements and new features

---
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerCachedPosMixin.java
@@ -0,0 +1,30 @@
+package me.fzzyhmstrs.particle_core.mixins;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fzzyhmstrs.particle_core.interfaces.BlockPosStorer;
+import me.fzzyhmstrs.particle_core.plugin.PcConditionTester;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Environment(EnvType.CLIENT)
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.TESTER, tester = PcConditionTester.class)
+        }
+)
+@Mixin(ParticleManager.class)
+public class ParticleManagerCachedPosMixin {
+
+    @WrapOperation(method = "tickParticle", at = @At(value = "INVOKE", target = "net/minecraft/client/particle/Particle.tick ()V"))
+    private void particle_core_tickParticlePositions(Particle instance, Operation<Void> original) {
+        ((BlockPosStorer) instance).particle_core_tickCachedPos();
+        original.call(instance);
+    }
+}
