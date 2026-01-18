1ddf8bac88da2703b360ea903d8c5776a1866053
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-18 00:30:03 -0500

    0.3.0 content. Many performance improvements and new features
    (cherry picked from commit b2845091a264d9c7a5ca40762b467e8cd90c23be)

---
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/BillboardParticleVertexMixin.java
@@ -0,0 +1,69 @@
+package me.fzzyhmstrs.particle_core.mixins;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fzzyhmstrs.particle_core.VertexContainer;
+import me.fzzyhmstrs.particle_core.interfaces.ParticleVertexer;
+import me.fzzyhmstrs.particle_core.plugin.PcConditionTester;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.BillboardParticle;
+import net.minecraft.client.render.VertexConsumer;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Environment(EnvType.CLIENT)
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.TESTER, tester = PcConditionTester.class)
+        }
+)
+@Mixin(value = BillboardParticle.class, priority = 100000)
+@Debug(export = true)
+abstract class BillboardParticleVertexMixin {
+
+    @WrapOperation(method = "buildGeometry", at = @At(value = "INVOKE", target = "net/minecraft/client/render/VertexConsumer.vertex(DDD)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer particle_core_gatherVertexVertexes(VertexConsumer instance, double x, double y, double z, Operation<VertexConsumer> original, @Share("vertex_container") LocalRef<VertexContainer> ref) {
+        VertexContainer c = new VertexContainer();
+        c.x = (float) x;
+        c.y = (float) y;
+        c.z = (float) z;
+        ref.set(c);
+        return instance;
+    }
+
+    @WrapOperation(method = "buildGeometry", at = @At(value = "INVOKE", target = "net/minecraft/client/render/VertexConsumer.texture (FF)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer particle_core_gatherVertexTextures(VertexConsumer instance, float u, float v, Operation<VertexConsumer> original, @Share("vertex_container") LocalRef<VertexContainer> ref) {
+        VertexContainer c = ref.get();
+        c.u = u;
+        c.v = v;
+        return instance;
+    }
+
+    @WrapOperation(method = "buildGeometry", at = @At(value = "INVOKE", target = "net/minecraft/client/render/VertexConsumer.color (FFFF)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer particle_core_gatherVertexColors(VertexConsumer instance, float red, float green, float blue, float alpha, Operation<VertexConsumer> original, @Share("vertex_container") LocalRef<VertexContainer> ref) {
+        VertexContainer c = ref.get();
+        c.red = red;
+        c.green = green;
+        c.blue = blue;
+        c.alpha = alpha;
+        return instance;
+    }
+
+    @WrapOperation(method = "buildGeometry", at = @At(value = "INVOKE", target = "net/minecraft/client/render/VertexConsumer.light (I)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer particle_core_gatherVertexLight(VertexConsumer instance, int i, Operation<VertexConsumer> original, @Share("vertex_container") LocalRef<VertexContainer> ref) {
+        VertexContainer c = ref.get();
+        c.light = i;
+        return instance;
+    }
+    @WrapOperation(method = "buildGeometry", at = @At(value = "INVOKE", target = "net/minecraft/client/render/VertexConsumer.next ()V"))
+    private void particle_core_performVertexing(VertexConsumer instance, Operation<Void> original, @Share("vertex_container") LocalRef<VertexContainer> ref) {
+        VertexContainer c = ref.get();
+        ((ParticleVertexer) instance).particle_core_particleVertex(c.x, c.y, c.z, c.red, c.green, c.blue, c.alpha, c.u, c.v, c.light);
+    }
+}
---
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleManagerAsyncMixin.java
@@ -0,0 +1,132 @@
+package me.fzzyhmstrs.particle_core.mixins;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fzzyhmstrs.particle_core.PcConfig;
+import me.fzzyhmstrs.particle_core.SynchronizedIdentityHashMap;
+import me.fzzyhmstrs.particle_core.plugin.PcConditionTester;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleGroup;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.particle.ParticleTextureSheet;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+@Environment(EnvType.CLIENT)
+@Restriction(
+		require = {
+				@Condition(type = Condition.Type.TESTER, tester = PcConditionTester.class)
+		}
+)
+@Mixin(value = ParticleManager.class, priority = 100000)
+@Debug(export = true)
+public abstract class ParticleManagerAsyncMixin {
+
+	@Shadow @Final private Map<ParticleTextureSheet, Queue<Particle>> particles;
+
+	@Shadow protected ClientWorld world;
+
+	@Shadow protected abstract void tickParticle(Particle particle);
+
+	@Shadow protected abstract void addTo(ParticleGroup group, int count);
+
+	@Shadow protected abstract void tickParticles(Collection<Particle> particles);
+
+	@WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "com/google/common/collect/Maps.newIdentityHashMap ()Ljava/util/IdentityHashMap;"))
+	private IdentityHashMap<?, ?> particle_core_setupSynchronizedParticleMap(Operation<IdentityHashMap<?, ?>> original) {
+		return new SynchronizedIdentityHashMap<>(original.call());
+	}
+
+	@SuppressWarnings({"SynchronizeOnNonFinalField"})
+	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "java/util/Map.forEach (Ljava/util/function/BiConsumer;)V"))
+	private void particle_core_asyncParticleTicking(Map<ParticleTextureSheet, Queue<Particle>> instance, BiConsumer<? super ParticleTextureSheet, ? extends Queue<Particle>> v, Operation<Void> original) {
+		if (!PcConfig.INSTANCE.getImpl().getAsynchronousTicking().get()) {
+			original.call(instance, v);
+		} else {
+			try {
+				var entries = this.particles.entrySet();
+				synchronized (this.particles) {
+					List<CompletableFuture<Collection<Particle>>> futures = new ArrayList<>(entries.size());
+					List<CompletableFuture<Collection<Particle>>> syncFutures = new ArrayList<>(entries.size());
+					float threshold = PcConfig.INSTANCE.getImpl().getMaxParticlesPerSheet().get() * 0.35f;
+					for (Map.Entry<ParticleTextureSheet, Queue<Particle>> entry : entries) {
+						this.world.getProfiler().push(entry.getKey().toString());
+						if (entry.getValue().isEmpty()) {
+							continue;
+						}
+						if (threshold < entry.getValue().size()) {
+							futures.add(CompletableFuture.supplyAsync(() -> asyncTickParticles(entry.getValue())));
+						}
+					}
+					for (Map.Entry<ParticleTextureSheet, Queue<Particle>> entry : entries) {
+						if (entry.getValue().isEmpty()) {
+							syncFutures.add(CompletableFuture.completedFuture(List.of()));
+							continue;
+						}
+						if (threshold >= entry.getValue().size()) {
+							syncFutures.add(CompletableFuture.completedFuture(syncTickParticles(entry.getValue())));
+						}
+					}
+					for (CompletableFuture<Collection<Particle>> future : syncFutures) {
+						finalizeParticles(future.join());
+						this.world.getProfiler().pop();
+					}
+
+					CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{})).join();
+					for (CompletableFuture<Collection<Particle>> future : syncFutures) {
+						finalizeParticles(future.join());
+						this.world.getProfiler().pop();
+					}
+				}
+			} catch (ConcurrentModificationException e) {
+				PcConfig.INSTANCE.getLogger().error("Asynchronous particle ticking encountered a concurrency problem; disabling");
+				PcConfig.INSTANCE.getImpl().getAsynchronousTicking().validateAndSet(false);
+			} catch (Exception e) {
+				throw new RuntimeException("Unhandled exception while ticking particles", e);
+			}
+		}
+	}
+
+	@Unique
+	private Collection<Particle> syncTickParticles(Collection<Particle> particles) {
+		this.tickParticles(particles);
+		return List.of(); //empty list because we've already fully process particles synchronously
+	}
+
+	@Unique
+	private Collection<Particle> asyncTickParticles(Collection<Particle> particles) {
+		particles.parallelStream().peek(this::tickParticle).forEach((p) -> {});
+		return particles;
+	}
+
+	@Unique
+	private void finalizeParticles(Collection<Particle> particles) {
+		Iterator<Particle> iterator = particles.iterator();
+		while (iterator.hasNext()) {
+			Particle particle = iterator.next();
+			if (particle.isAlive()) continue;
+			particle.getGroup().ifPresent(group -> this.addTo(group, -1));
+			iterator.remove();
+		}
+	}
+}
