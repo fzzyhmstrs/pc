0edf31b2e2420ad3e73cb71c799cbf40662953a2
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-19 16:36:46 -0500

    0.3.0 Add culling blacklist, culling options, config polishing

--- src/main/kotlin/me/fzzyhmstrs/particle_core/PcConfigImpl.kt
+++ src/main/kotlin/me/fzzyhmstrs/particle_core/PcConfigImpl.kt
@@ -1,10 +1,11 @@
 package me.fzzyhmstrs.particle_core
 
-import me.fzzyhmstrs.fzzy_config.annotations.Action
-import me.fzzyhmstrs.fzzy_config.annotations.ConvertFrom
-import me.fzzyhmstrs.fzzy_config.annotations.IgnoreVisibility
-import me.fzzyhmstrs.fzzy_config.annotations.RequiresAction
+import com.google.common.collect.Sets
+import me.fzzyhmstrs.fzzy_config.annotations.*
 import me.fzzyhmstrs.fzzy_config.config.Config
+import me.fzzyhmstrs.fzzy_config.entry.EntryFlag
+import me.fzzyhmstrs.fzzy_config.validation.ValidatedField.Companion.withListener
+import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedChoiceList
 import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedIdentifierMap
 import me.fzzyhmstrs.fzzy_config.validation.minecraft.ValidatedIdentifier
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedBoolean
@@ -13,45 +14,88 @@ import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedDouble
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedFloat
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber.Companion.withIncrement
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.particle.Particle
+import net.minecraft.client.render.Frustum
 import net.minecraft.particle.ParticleType
 import net.minecraft.particle.ParticlesMode
 import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.MathHelper
-import net.minecraft.util.math.Vec3d
+import java.util.function.Predicate
 
 @ConvertFrom("particle_core_config_v1.json")
 @IgnoreVisibility
+@Version(1)
 class PcConfigImpl: Config(Identifier.of("particle_core","particle_core_config"), "","") {
 
-    var turnOffPotionParticles = ValidatedEnum(PcConfig.PotionDisableType.NONE)
+    override fun update(deserializedVersion: Int) {
+        if (deserializedVersion == 0) {
+            val l = turnOffPotionParticles.get().mapTo.toList()
+            turnOffPotionParticlesV2.validateAndSetFlagged(l, EntryFlag.Flag.QUIET)
+            PcConfig.previousChoices = l
+        }
+    }
 
-    private var reduceAllChance = ValidatedFloat(0f, 1f, 0f)
-
-    private var reduceDecreasedChance = ValidatedFloat(0f, 1f, 0f)
+    @Deprecated("No longer used")
+    @ConfigDeprecated
+    private var turnOffPotionParticles = ValidatedEnum(PcConfig.DeprecatedType.NONE)
 
     var disableParticles = ValidatedBoolean(false)
 
-    var byTypeReductions = ValidatedIdentifierMap(mapOf(), ValidatedIdentifier.ofRegistry(Identifier.of("smoke"), Registries.PARTICLE_TYPE), ValidatedDouble(1.0, 1.0, 0.0))
+    private var byTypeReductions = ValidatedIdentifierMap(mapOf(), ValidatedIdentifier.ofRegistry(Identifier.of("smoke"), Registries.PARTICLE_TYPE), ValidatedDouble(1.0, 1.0, 0.0))
 
     @RequiresAction(Action.RESTART)
-    var maxParticlesPerSheet = ValidatedInt(16384, Int.MAX_VALUE, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
+    var maxParticlesPerSheet = ValidatedInt(16384, Int.MAX_VALUE, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS).withIncrement(1000)
 
     private var particleRenderDistanceMultiplier = ValidatedDouble(1.0, 1.0, 0.1, ValidatedNumber.WidgetType.SLIDER)
 
     var asynchronousTicking = ValidatedBoolean()
 
+    private var cullingBlacklist = ValidatedIdentifier.ofRegistry(Identifier("smoke"), Registries.PARTICLE_TYPE).toSet()
+
+    private var cullingBehavior = ValidatedEnum(PcConfig.CullingBehavior.AGGRESSIVE)
+
+    private var reduceAllChance = ValidatedFloat(0f, 1f, 0f)
+
+    private var reduceDecreasedChance = ValidatedFloat(0f, 1f, 0f)
+
+    private var turnOffPotionParticlesV2 = ValidatedEnum(PcConfig.PotionDisableType.NONE)
+        .toList(PcConfig.PotionDisableType.entries)
+        .toChoiceList(listOf(PcConfig.PotionDisableType.NONE), ValidatedChoiceList.WidgetType.INLINE)
+        .withListener { choices ->
+            val filter: Predicate<PcConfig.PotionDisableType> = Sets.difference(choices.toMutableSet(), PcConfig.previousChoices.toMutableSet()).fold(Predicate { true }) { p, c ->
+                p.and(c.choicePredicate)
+            }
+            var actualChoices = choices.stream().filter(filter).toList()
+            if (actualChoices.isEmpty()) actualChoices = listOf(PcConfig.PotionDisableType.NONE)
+            if (actualChoices != choices.get()) {
+                choices.validateAndSetFlagged(actualChoices, EntryFlag.Flag.QUIET)
+            }
+            PcConfig.previousChoices = ArrayList(choices.get())
+            var flags = 0
+            for (choice in choices) {
+                flags = choice.flags or flags
+            }
+            PcConfig.choiceFlags = flags
+        }
+
     fun setupParticleViewDistance() {
         PcConfig.renderDistance = MathHelper.square(MinecraftClient.getInstance().options.clampedViewDistance * 16 * particleRenderDistanceMultiplier.get())
     }
 
-    fun shouldRenderParticle(x: Double, y: Double, z: Double, pos: Vec3d): Boolean {
-        return pos.squaredDistanceTo(x, y, z) <= PcConfig.renderDistance
+    fun shouldSpawnParticle(type: ParticleType<*>): Boolean {
+        val chance = byTypeReductions[Registries.PARTICLE_TYPE.getId(type) ?: return true] ?: return true
+        return PcUtils.random.nextDouble() < chance
     }
 
-    fun shouldSpawnParticle(type: ParticleType<*>): Boolean {
-        val chance = PcConfig.byTypeParticleReduction[Registries.PARTICLE_TYPE.getId(type) ?: return true] ?: return true
-        return PcUtils.random.nextDouble() < chance
+    fun keepParticle(frustum: Frustum, particle: Particle): Boolean {
+        return cullingBehavior.get().shouldKeep(frustum, particle)
+    }
+
+    fun shouldBlacklistParticle(type: ParticleType<*>): Boolean {
+        return cullingBlacklist.contains(Registries.PARTICLE_TYPE.getId(type) ?: return false)
     }
 
     fun getReducedParticleSpawnType(mode: ParticlesMode): ParticlesMode {
