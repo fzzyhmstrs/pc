befc8c5c2d7972cc288a1ebd149b7f5fe267d718
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-19 21:28:13 -0500

    0.3.0 1.20.1-1.21.8 get missing disable mixin

--- src/main/kotlin/me/fzzyhmstrs/particle_core/PcDisable.kt
+++ src/main/kotlin/me/fzzyhmstrs/particle_core/PcDisable.kt
@@ -48,9 +48,9 @@ object PcDisable {
             "CULLING" to "[Impact: HIGH] Disables mixins related to particle culling (FrustumAccessor, ParticleAccessor, ParticleManagerFrustumMixin, WorldRendererFrustumMixin, ParticleFrustumBlacklistMixin)",
             "TYPE" to "[Impact: Low to Medium] Disables mixins related to particle disabling and reduction (WorldRendererTypeMixin)",
             "DECREASE" to "[Impact: Low] Disables mixins related particle settings reduction (ALL, DECREASED, MINIMAL) (WorldRendererDecreaseMixin)",
-            "LIGHTMAP" to "[Impact: Medium] Disables mixins related to light map caching (ParticleManagerCachedLightMixin, ParticleBrightnessCacheMixin, ParticleCachePosMixin)",
+            "LIGHTMAP" to "[Impact: Medium] Disables mixins related to light map caching (ParticleManagerCachedLightMixin, ParticleBrightnessCacheMixin, ParticleCachePosMixin, ParticleManagerCachedPosMixin)",
             "POTION" to "[Impact: Low] Disables mixins related to potion particle disabling (LivingEntityMixin)",
-            "MOVE" to "[Impact: Medium] Disables mixins related to particle movement optimization (ParticleMoveAdjustMixin, ParticleCachePosMixin)",
+            "MOVE" to "[Impact: Medium] Disables mixins related to particle movement optimization (ParticleMoveAdjustMixin, ParticleCachePosMixin, ParticleManagerCachedPosMixin)",
             "VERTEX" to "[Impact: HIGH] Disables mixins related to particle vertex drawing optimizations (BillboardParticleVertexMixin, BufferBuilderVertexMixin)",
             "COUNT" to "[Impact: Variable] Disables mixins related to max particle count setting (ParticleManagerCountMixin)",
             "ASYNC" to "[Impact: Medium] Disables asynchronous ticking of particles (ParticleManagerAsyncMixin)",
@@ -119,7 +119,8 @@ object PcDisable {
                     return true
                 }
                 if(disableOptimizations.contains("LIGHTMAP")) {
-                    if (className.endsWith("ParticleCachePosMixin"))
+                    if (className.endsWith("ParticleCachePosMixin")
+                        || className.endsWith("ParticleManagerCachedPosMixin"))
                     {
                         println("Disabling [$className] due to 'MOVE' and 'LIGHTMAP' key in particle core config!")
                         return true
