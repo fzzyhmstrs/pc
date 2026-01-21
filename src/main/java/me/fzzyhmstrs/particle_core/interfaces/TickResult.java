70a65890bc8e64388359fe624b5336caf7c2d470
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2026-01-21 11:19:59 -0500

    0.3.1 create TickResult to hold async results

---
+++ src/main/java/me/fzzyhmstrs/particle_core/interfaces/TickResult.java
@@ -0,0 +1,6 @@
+package me.fzzyhmstrs.particle_core.interfaces;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
+public record TickResult(boolean success, Particle particle)
