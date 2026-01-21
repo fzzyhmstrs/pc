adaeddabba2b1cc9a64383b55ba5d2f72bae5a85
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-20 20:08:33 -0500

    0.3.0 NEO 1.21.4 get build working

---
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleAccessor.java
@@ -0,0 +1,17 @@
+package me.fzzyhmstrs.particle_core.mixins;
+
+import net.minecraft.client.particle.Particle;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Particle.class)
+public interface ParticleAccessor {
+	@Accessor
+	double getX();
+
+	@Accessor
+	double getZ();
+
+	@Accessor
+	double getY();
+}
