9200aee721afbf293479379b0d2ff9ae8d6c3713
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2026-01-21 13:38:40 -0500

    0.3.1 unrevert revert of TickResult :/

--- src/main/java/me/fzzyhmstrs/particle_core/interfaces/TickResult.java
+++ src/main/java/me/fzzyhmstrs/particle_core/interfaces/TickResult.java
@@ -1,9 +1,10 @@
 package me.fzzyhmstrs.particle_core.interfaces;
 
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleRenderer;
 
 import java.util.Collection;
 
 public record TickResult(boolean failure, Particle particle) {
-    public record Results(Collection<TickResult> results, Collection<? extends Particle> originalCollection) {}
+    public record Results(Collection<TickResult> results, ParticleRenderer<?> originalCollection) {}
 }
