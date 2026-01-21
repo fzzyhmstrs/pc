34b9fe439ee8286c005fb356c803e94cc6a22ab8
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2026-01-21 11:36:58 -0500

    0.3.1 update TickResult success bl to failure

--- src/main/java/me/fzzyhmstrs/particle_core/interfaces/TickResult.java
+++ src/main/java/me/fzzyhmstrs/particle_core/interfaces/TickResult.java
@@ -3,4 +3,4 @@ package me.fzzyhmstrs.particle_core.interfaces;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 
-public record TickResult(boolean success, Particle particle)
+public record TickResult(boolean failure, Particle particle)
