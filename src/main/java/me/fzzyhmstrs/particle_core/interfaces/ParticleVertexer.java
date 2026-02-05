5b243d616bdb24456fa298d45968acab516dd330
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-02-05 14:21:15 -0500

    0.3.3

--- src/main/java/me/fzzyhmstrs/particle_core/interfaces/ParticleVertexer.java
+++
@@ -1,5 +0,0 @@
-package me.fzzyhmstrs.particle_core.interfaces;
-
-public interface ParticleVertexer {
-	void particle_core_particleVertex(float x, float y, float z, float red, float green, float blue, float alpha, float u, float v, int light);
-}
