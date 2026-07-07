e466db73b8ebeffefeaf1bd6751a8b3c283d9b95
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-07-07 19:13:06 -0400

    26.2
    (cherry picked from commit 1151fe6aca4e1c3b62459de3e3a99ec32af2ac99)

--- src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleGroupAccessor.java
+++ src/main/java/me/fzzyhmstrs/particle_core/mixins/ParticleGroupAccessor.java
@@ -3,10 +3,16 @@ package me.fzzyhmstrs.particle_core.mixins;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleGroup;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.Queue;
 
 @Mixin(ParticleGroup.class)
 public interface ParticleGroupAccessor {
 	@Invoker
 	void callTickParticle(Particle particle);
+
+	@Accessor
+	Queue<? extends Particle> getParticles();
 }
